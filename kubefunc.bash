405ede2641725aa3141b07efbe2e169ef2d2da2e
Dan Isla <dan.isla@gmail.com>
2022-02-28 04:03:15 +0000

    fix install-krew function

--- kubefunc.bash
+++ kubefunc.bash
@@ -66,10 +66,12 @@ function install-krew() {
   fi
   (
     set -x; cd "$(mktemp -d)" &&
-    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
-    tar zxvf krew.tar.gz &&
-    KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/' -e 's/aarch64$/arm64/')" &&
-    "$KREW" install krew
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    KREW="krew-${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf ${KREW}.tar.gz &&
+    ./"$KREW" install krew
   )
 }
 
